3e9de785c596f35a06c9e0801d329d3f8dd37c0e
Matt Cestarte <matthew.cestarte@runbox.com>
2021-11-14 14:02:54 -0600

    Ch.1 VAT

---
+++ 1/vat/Program.cs
@@ -0,0 +1,46 @@
+﻿Product p = new(false);
+Order o = new(10, p);
+Address a = new("jp");
+Console.WriteLine(Vat(a, o));
+
+UsAddress usa = new("ma");
+Console.WriteLine(Vat(usa, o));
+
+static decimal Vat(Address address, Order order)
+   => address switch
+   {
+     UsAddress(var state) => VatByRate(RateByState(state), order),
+     ("de") _ => DeVat(order),
+     (var country) _ => VatByRate(RateByCountry(country), order),
+   };
+
+
+static decimal VatByRate(decimal rate, Order order)
+   => order.NetPrice * rate;
+
+static decimal DeVat(Order order)
+   => order.NetPrice * (order.Product.IsFood ? 0.08m : 0.2m);
+
+static decimal RateByCountry(string country)
+   => country switch
+   {
+     "it" => 0.22m,
+     "jp" => 0.08m,
+     _ => throw new ArgumentException($"Missing rate for {country}")
+   };
+
+static decimal RateByState(string state)
+   => state switch
+   {
+     "ca" => 0.1m,
+     "ma" => 0.0625m,
+     "ny" => 0.085m,
+     _ => throw new ArgumentException($"Missing rate for {state}")
+   };
+
+record Product(bool IsFood);
+record Order(decimal NetPrice, Product Product);
+record Address(string Country);
+record UsAddress(string State) : Address("us");
+
+
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Examples and Exercises from Functional Programming in C#, Second Edition By Enrico Buonanno
+
+A place to organize examples and exercises as I work through the MEAP.
+
+## [Pre-order the book from Manning.com](https://www.manning.com/books/functional-programming-in-c-sharp-second-edition)
+
+## Requirements
+
+  * [.net 6 SDK](https://dotnet.microsoft.com/download/dotnet/6.0)
+  * [Visual Studio Code](https://code.visualstudio.com/Download)
+
+## Notes
+  * At the time of this writing, Visual Studio Code's C# extension doesn't seem happy with some of the top level statements with .net 6 but the code compiles and runs.
