ffc4938630416ae1156fe02b33d778ba88fbbf4e
Matt Cestarte <matthew.cestarte@runbox.com>
2021-11-16 12:49:00 -0600

    ch2. lambda improvements in C#10

---
+++ 2/lambda-improvements/Program.cs
@@ -0,0 +1,5 @@
+﻿Func<int, int, int> divideClunky = (x, y) => x / y;
+var divideStreamlined = (int x, int y) => x / y;
+
+Console.WriteLine($"Clunky 10 / 2 = {divideClunky(10, 2)}");
+Console.WriteLine($"Streamlined 10 / 2 = {divideStreamlined(10, 2)}");
